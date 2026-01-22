a8bbfdc99b8768738fb747bd815712816a6bcfe0
skarndev <skarnproject@gmail.com>
2026-01-22 21:45:34 +0300

    fix spelling error

--- tests/py-demo/bindings/src/modules/hidden_builtins.cpp
+++ tests/py-demo/bindings/src/modules/hidden_builtins.cpp
@@ -5,8 +5,7 @@
 void bind_hidden_builtins_module(py::module &&m) {
   py::dict readonly_dict{};
 
-  auto mappingproxy_t = py::object types =
-      py::module::import("types").attr("MappingProxyType");
+  auto mappingproxy_t = py::module::import("types").attr("MappingProxyType");
 
   m.attr("mapping_proxy") = mappingproxy_t(readonly_dict);
 }
