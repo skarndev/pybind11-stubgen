91edb5635e97aa1a0a9a3e752a6919761a2867ff
skarndev <skarnproject@gmail.com>
2026-01-22 21:34:18 +0300

    add generation of mapping proxy test case

--- tests/py-demo/bindings/src/modules.h
+++ tests/py-demo/bindings/src/modules.h
@@ -1,22 +1,25 @@
 #pragma once
-#include <pybind11/pybind11.h>
 #include "opaque_types.h"
+#include <pybind11/pybind11.h>
 
-#define PYBIND11_VERSION_AT_LEAST(x,y) (PYBIND11_VERSION_MAJOR>x || ( PYBIND11_VERSION_MAJOR>=x && PYBIND11_VERSION_MINOR>=y ))
+#define PYBIND11_VERSION_AT_LEAST(x, y)                                        \
+  (PYBIND11_VERSION_MAJOR > x ||                                               \
+   (PYBIND11_VERSION_MAJOR >= x && PYBIND11_VERSION_MINOR >= y))
 
 namespace py = pybind11;
 
-void bind_aliases_module(py::module&& m);
-void bind_classes_module(py::module&& m);
-void bind_eigen_module(py::module&& m);
-void bind_enum_module(py::module&& m);
-void bind_flawed_bindings_module(py::module&& m);
-void bind_functions_module(py::module&& m);
-void bind_issues_module(py::module&& m);
-void bind_methods_module(py::module&& m);
-void bind_numpy_module(py::module&& m);
-void bind_properties_module(py::module&& m);
-void bind_stl_module(py::module&& m);
-void bind_stl_bind_module(py::module&& m);
-void bind_typing_module(py::module&& m);
-void bind_values_module(py::module&& m);
+void bind_aliases_module(py::module &&m);
+void bind_classes_module(py::module &&m);
+void bind_eigen_module(py::module &&m);
+void bind_enum_module(py::module &&m);
+void bind_flawed_bindings_module(py::module &&m);
+void bind_functions_module(py::module &&m);
+void bind_issues_module(py::module &&m);
+void bind_methods_module(py::module &&m);
+void bind_numpy_module(py::module &&m);
+void bind_properties_module(py::module &&m);
+void bind_stl_module(py::module &&m);
+void bind_stl_bind_module(py::module &&m);
+void bind_typing_module(py::module &&m);
+void bind_values_module(py::module &&m);
+void bind_hidden_builtins_module(py::module &&m);
---
+++ tests/py-demo/bindings/src/modules/hidden_builtins.cpp
@@ -0,0 +1,10 @@
+#include "modules.h"
+
+void bind_hidden_builtins_module(py::module &&m) {
+  py::dict readonly_dict{};
+
+  auto mappingproxy_t = py::object types =
+      py::module::import("types").attr("MappingProxyType");
+
+  m.attr("mapping_proxy") = mappingproxy_t(readonly_dict);
+}
