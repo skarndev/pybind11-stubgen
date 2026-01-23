3740dddeb0edb44b8f3815e80c7ad047b4dc2219
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-01-23 21:43:53 +0000

    Remove Python 3.7 tests (#285)
    I think these should have been removed in #284

--- tests/stubs/python-3.7/pybind11-v2.13/numpy-array-wrap-with-annotated/demo/_bindings/methods.pyi
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-__all__: list[str] = ["Dummy"]
-
-class Dummy:
-    @staticmethod
-    def static_method(arg0: int) -> int: ...
-    def regular_method(self, arg0: int) -> int: ...
