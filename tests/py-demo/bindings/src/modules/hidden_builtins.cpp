a2df17279ed24ae5b4501cc8db2644db2dfc7d11
skarndev <skarnproject@gmail.com>
2026-01-29 12:37:50 +0300

    add test case for None

--- tests/py-demo/bindings/src/modules/hidden_builtins.cpp
+++ tests/py-demo/bindings/src/modules/hidden_builtins.cpp
@@ -2,10 +2,17 @@
 
 #include <pybind11/pytypes.h>
 
+namespace hidden_builtins_detail {
+
+};
+
 void bind_hidden_builtins_module(py::module&& m) {
   py::dict readonly_dict{};
 
-  auto mappingproxy_t = py::module::import("types").attr("MappingProxyType");
+  auto types_m = py::module::import("types");
+  auto mappingproxy_t = types_m.attr("MappingProxyType");
+  auto none_t = types_m.attr("NoneType");
 
   m.attr("mapping_proxy") = mappingproxy_t(readonly_dict);
+  m.attr("none") = none_t();
 }
--- tests/stubs/python-3.12/pybind11-v2.13/numpy-array-wrap-with-annotated/demo/_bindings/hidden_builtins.pyi
+++ tests/stubs/python-3.12/pybind11-v2.13/numpy-array-wrap-with-annotated/demo/_bindings/hidden_builtins.pyi
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 import types
 
-__all__: list[str] = ["mapping_proxy"]
+__all__: list[str] = ["mapping_proxy", "none"]
 mapping_proxy: types.MappingProxyType  # value = mappingproxy({})
+none = None
